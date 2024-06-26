523ab22f6e2a57a9f62f22ebe0fb32654d619f4f
unknown <bgsdewoo20@gmail.com>
2024-06-26 12:09:36 +0700

    Membuat Ui Jadi Dinamis Dengan State Dan Event

--- steps-form/src/App.js
+++ steps-form/src/App.js
@@ -1,19 +1,33 @@
+import { useState } from "react";
 function App() {
+  //menggunakan variabel
+  // let step = 1;
+
+  //menggunakan use state
+  const [step, setStep] = useState(1);
   function handleNext() {
-    alert("Next Button clicked");
+    //menggunakan variabel
+    // step += 1;
+    // console.log(step);
+    //menggunakan use state
+    if (step < 3) setStep(step + 1);
+    console.log(step);
+  }
+  function handlePrev() {
+    if (step > 1) setStep(step - 1);
   }
   return (
     <div className="steps">
       <div className="numbers">
-        <div className="active">1</div>
-        <div className="">2</div>
-        <div className="">3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">Step : Dream</p>
       <div className="buttons">
         <button
           style={{ backgroundColor: "#526D82", color: "#fff" }}
-          onMouseEnter={() => alert("prev hover")}
+          onClick={handlePrev}
         >
           Prev
         </button>
