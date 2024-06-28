264bd8c20f9c4f32c46311b7e29bf28d256d409d
unknown <bgsdewoo20@gmail.com>
2024-06-28 16:01:15 +0700

    Mengurutkan Deret Data Melalui State

--- gocheck/src/App.js
+++ gocheck/src/App.js
@@ -71,10 +71,24 @@ function Form({ onAddItem }) {
 }
 
 function CheckList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+  function sortItems() {
+    switch (sortBy) {
+      case "title":
+        return items.slice().sort((a, b) => a.title.localeCompare(b.title));
+      case "status":
+        return items.slice().sort((a, b) => Number(a.done) - Number(b.done));
+      case "input":
+      default:
+        return items;
+    }
+  }
+
+  const sortedItems = sortItems();
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -83,6 +97,13 @@ function CheckList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Urutkan berdasarkan input</option>
+          <option value="title">Urutkan berdasarkan judul</option>
+          <option value="status">Urutkan berdasarkan status</option>
+        </select>
+      </div>
     </div>
   );
 }
